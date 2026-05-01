c3053a53fab9e22547c17a68a1697fd9f0deb3b9
phiner <phiner@gmail.com>
2026-05-01 15:28:24 -0500

    feat: add external holdout dataset

--- cajas/datasets/__init__.py
+++ cajas/datasets/__init__.py
@@ -1,5 +1,6 @@
 """Dataset adapters for cajas research workflows."""
 
 from .prepared_dataset import PreparedDataset
+from .external_holdout_dataset import ExternalHoldoutDataset, ExternalHoldoutDatasetSummary
 
-__all__ = ["PreparedDataset"]
+__all__ = ["PreparedDataset", "ExternalHoldoutDataset", "ExternalHoldoutDatasetSummary"]
