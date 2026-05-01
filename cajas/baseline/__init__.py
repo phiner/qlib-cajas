c7b897bb6d0d406e0c973c684dddf388fccc1e1d
phiner <phiner@gmail.com>
2026-05-01 11:18:24 -0500

    feat: add training-disabled baseline plan

---
+++ cajas/baseline/__init__.py
@@ -0,0 +1,5 @@
+"""Training-disabled baseline planning helpers."""
+
+from .baseline_plan import BaselineModelSpec, BaselinePlanReport, build_baseline_plan
+
+__all__ = ["BaselineModelSpec", "BaselinePlanReport", "build_baseline_plan"]
